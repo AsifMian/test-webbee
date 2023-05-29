91946d5ffc3150f71dacd0e606df12520216656b
Asif <asif43997@gmail.com>
2023-05-30 00:43:24 +0500

    Task 3

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -93,6 +93,8 @@ class MenuController extends BaseController
      */
 
     public function getMenuItems() {
-        throw new \Exception('implement in coding task 3');
+        $menuItems = MenuItem::getMenuItems();
+        
+        return response()->json($menuItems);
     }
 }
--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -9,4 +9,16 @@ use Illuminate\Database\Eloquent\Model;
 class MenuItem extends Model
 {
 
+    public function workshops() {
+        return $this->hasMany(Workshop::class,'id','parent_id');
+    }
+
+    public static function getMenuItems() {
+        return self::with('children')->whereNull('parent_id')->get();
+    }
+
+    public function children() {
+        return $this->hasMany(MenuItem::class, 'parent_id')->with('children');
+    }
+
 }
